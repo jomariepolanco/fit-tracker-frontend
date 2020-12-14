d84f7be58fcc15366a0dfc95bdb68ae35a61c4b1
jomariepolanco <jomariepolanco@gmail.com>
2020-12-13 20:21:15 -0500

    workouts and exercises index pages done

--- src/index.js
+++ src/index.js
@@ -8,9 +8,61 @@ const motivation = document.querySelector(".motivation")
 const userCard = document.querySelector(".user")
 const statsCard = document.querySelector(".stats")
 const workoutCalendar = document.querySelector(".workouts")
-/**** EVENT LISTENERS/HANDLERS ****/
+const statsForm = document.querySelector("#stats-form")
+const header = document.querySelector("header")
+const workoutsMain = document.querySelector("#workouts-main")
+const allWo = document.querySelector(".all-workouts")
+const myWorkouts = document.querySelector(".my-workouts")
+const allExercises = document.querySelector(".all-exercises")
+const exerciseMain = document.querySelector("#exercises-main")
+const workoutForm = document.querySelector(".create-new-workouts")
 
+/**** EVENT LISTENERS/HANDLERS ****/
+header.addEventListener("click", event => {
+    if (event.target.tagName === "A"){
+        if (event.target.textContent === "Workouts"){
+            workoutsMain.style.display = ""
+            renderAllWorkouts()
+            main.style.display = "none"
+            exerciseMain.style.display = "none"
+        } else if (event.target.textContent === "Exercises"){
+            exerciseMain.style.display = ""
+            main.style.display = "none"
+            workoutsMain.style.display = "none"
+            renderExercisesPage()
+        } else if (event.target.textContent === "Create a Workout"){
+            exerciseMain.style.display = "none"
+            main.style.display = "none"
+            workoutsMain.style.display = "none"
+            workoutForm.style.display = ""
+        }
+    }
+})
+
+
+statsForm.addEventListener("submit", event => {
+    event.preventDefault()
+    const newStatsObj = {
+        user_id: user.id,
+        exercise_id: parseInt(event.target.exercise.value),
+        weight: parseInt(event.target.weight.value),
+        comment: event.target.comment.value
+    }
+    console.log(newStatsObj)
+    createNewStat(newStatsObj)
+    event.target.reset()
+})
 /**** FETCHES ****/
+
+const renderAllWorkouts = () => {
+    fetch(`${URL}/workouts`)
+    .then(r => r.json())
+    .then(workouts => {
+        workouts.forEach(workout => renderWorkout(workout))
+        const myWorkouts = workouts.filter(workout => workout.user_id == user.id)
+        renderMyWorkouts(myWorkouts)
+    })
+}
 
 const setUser = (username, password) => {
     return fetch(`${URL}/users`)
@@ -29,10 +81,67 @@ const fetchWorkoutAppointments = () => {
     .then(r => r.json())
     .then(appointments => renderWorkoutCalendar(appointments))
 }
+
+const fetchExercises = () => {
+    fetch(`${URL}/exercises`)
+    .then(r => r.json())
+    .then(exercises => renderStatsForm(exercises))
+}
+
+const createNewStat = (statObj) => {
+    fetch(`${URL}/stats`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(statObj)
+    })
+    .then(r => r.json())
+    .then(stat => console.log("success", stat))
+}
 /**** RENDER FUNCTIONS ****/
+
+renderExercisesPage = () => {
+    fetch(`${URL}/exercises`)
+    .then(r => r.json())
+    .then(exercises => {exercises.forEach(exercise => {
+        const div = document.createElement("div")
+        div.className = exercise.name
+        const h3 = document.createElement("h3")
+        h3.textContent = exercise.name  
+        const p = document.createElement("p")
+        p.innerHTML = `${exercise.description}`
+        div.append(h3, p)
+        allExercises.append(div)
+    })
+})
+}
+
+renderWorkout = (workout) => {
+    const div = document.createElement("div")
+    div.className = workout.kind
+    const h3 = document.createElement("h3")
+    h3.textContent = workout.kind 
+    const p = document.createElement("p")
+    p.textContent = workout.exercises.map(ex => `${ex.name}`).join(", ")
+    div.append(h3, p)
+    allWo.append(div)
+}
+
+const renderMyWorkouts = (workouts) => {
+    workouts.forEach(workout => {
+        const div = document.createElement("div")
+        div.className = workout.kind
+        const h3 = document.createElement("h3")
+        h3.textContent = workout.kind 
+        const p = document.createElement("p")
+        p.textContent = workout.exercises.map(ex => `${ex.name}`).join(", ")
+        div.append(h3, p)
+        myWorkouts.append(div)
+    })
+}
 
 const logIn = () => {
     main.style.display = "none"
+
     loginForm.addEventListener("submit", event => {
         event.preventDefault()
         const username = event.target.username.value
@@ -48,7 +157,7 @@ const renderMain = (users, username, password) => {
     renderUserInfo()
     fetchStats()
     fetchWorkoutAppointments()
-    renderStatsForm()
+    fetchExercises()
     loginForm.style.display = "none"
 }
 
@@ -90,11 +199,25 @@ const renderStats = (statsData) => {
         div.append(nameAndWeight, comment)
         statsCard.append(div)
     })
-    console.log(stats)
 }
 
-const renderStatsForm = () => {
-    console.log("stats form")
+const renderStatsForm = (exercises) => {
+    // console.log(exercises)
+    // const user = document.createElement("input")
+    // user.type = "text"
+    // user.name = "user"
+    // user.placeholder = "username"
+    const xDropDown = document.createElement("select")
+    xDropDown.name = "exercise"
+    exercises.forEach(exercise => {
+        const option = document.createElement("option")
+        option.name = exercise.name 
+        option.value = exercise.id 
+        option.textContent = exercise.name 
+        xDropDown.append(option)
+    })
+
+    statsForm.prepend(xDropDown)
 }
 
 const renderWorkoutCalendar = (appointments) => {
@@ -112,5 +235,4 @@ const renderWorkoutCalendar = (appointments) => {
 }
 
 /**** INITIALIZE ****/
-
 logIn()
